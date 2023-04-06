ca538efc61e1831d9100e23626b5386a5406ee70
Nikita <50362451+Fjornir@users.noreply.github.com>
2023-04-06 19:56:22 +0300

    Main section rework and slider added

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function triggerSubmitButtonClick() {
-    document.querySelector(".form-detail__button").dispatchEvent(new Event("click"));
-  }
+  document
+    .querySelector(".form-detail__button")
+    .dispatchEvent(new Event("click"));
+}
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
