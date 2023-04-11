5d999cc18ab96148ead24727b21838486a6ea3c2
Nikita <50362451+Fjornir@users.noreply.github.com>
2023-04-11 18:01:37 +0300

    Adaptive popup menu added and final css fixes

--- index.js
+++ index.js
@@ -7,24 +7,43 @@ function triggerSubmitButtonClick() {
 function personalMenuOpen() {
   let menu = document.querySelector(".popup");
   let actualMenuDisplay = getComputedStyle(menu).display;
-  actualMenuDisplay === "none"? menu.style.display = "block" : menu.style.display = "none" 
+  actualMenuDisplay === "none"
+    ? (menu.style.display = "block")
+    : (menu.style.display = "none");
 
   console.log(actualMenuDisplay);
 }
 
+function adaptiveMenuOpen() {
+  let menu = document.querySelector(".popup_adaptive");
+  let blur = document.querySelector(".blur");
+  let menuImage = document.querySelector("#adaptive_menu_image")
+  let scroll = document.getElementsByTagName('body')[0];
+  let actualMenuDisplay = getComputedStyle(menu).display;
+
+  if (actualMenuDisplay === "none") {
+    menuImage.src="./public/imgs/header/MenuClose.png";
+    menu.style.display = "flex";
+    blur.style.display = "block";
+    scroll.style.overflow = 'hidden'
+  } else {
+    menuImage.src="./public/imgs/header/MenuOpen.png";
+    menu.style.display = "none";
+    blur.style.display = "none";
+    scroll.style.overflow = 'visible'
+  }
+}
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     type: "bullets",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
